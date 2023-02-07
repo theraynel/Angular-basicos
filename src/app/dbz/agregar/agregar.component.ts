05b468db21308b7a26904d7e0666a69f0f7109a5
Raynel Feliz <feliz.r@dhn.com.do>
2023-02-07 19:45:12 -0400

    new changer

---
+++ src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+})
+export class AgregarComponent {
+
+  @Input() nuevo: Personaje = {
+    nombre: 'Trueno',
+    poder: 0
+  }
+
+  @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+
+  agregar() {
+     if(this.nuevo.nombre.trim() === '') return;
+     console.log(this.nuevo);
+
+     this.onNuevoPersonaje.emit(this.nuevo);
+
+     this.nuevo = { 
+      nombre: '',
+     poder: 0
+    }
+  }
+}
